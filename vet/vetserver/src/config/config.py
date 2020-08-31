66ea852067b1fb90f332b93d7671799779e09e68
Inquisitions <Inquisitions@github.com>
2020-09-01 01:24:10 +0300

    changes

--- vet/vetserver/src/config/config.py
+++ vet/vetserver/src/config/config.py
@@ -32,7 +32,7 @@ class Config(object):
 		return [str(self.__parser["ROUTES"]["animal_full_info"])]
 
 	def get_routes_visit_new(self):
-		return [str(self.__parser["ROUTES"]["new_visit"])]
+    		return [str(self.__parser["ROUTES"]["new_visit"])]
 
 	def get_routes_current_visits(self):
 		return [str(self.__parser["ROUTES"]["current_visits"])]
